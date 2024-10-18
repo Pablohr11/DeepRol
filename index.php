143850867d9aac040712bfde7b80450c2944d46e
Pablo <pablo993968@gmail.com>
2024-10-18 13:16:36 +0200

    Start

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DeepRol</title>
+    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/leftColumn.css">
+    <link rel="stylesheet" href="styles/footer.css">
+</head>
+<body>
+    <div id="header"></div>
+    <div id="content">
+        <div id="leftBar" class="dark"></div>
+        <div id="mainContent" class="dark"></div>
+    </div>
+    <div id="footer"></div>
+</body>
+</html>
