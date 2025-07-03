724604c919277a18700211bf560545bd94ab7527
Pablo <pablo993968@gmail.com>
2025-07-03 18:01:14 +0200

    sidebar coqueta

--- sections/_partials/leftColumn.php
+++ sections/_partials/leftColumn.php
@@ -1,7 +1,7 @@
 <div id="leftColumnContent">
     <img id="toogleSideBarButton" width="20px" height="20px"  src="../../resources/imgs/collapse.png" alt="">
-    <div class="lcOption title" id="Partidas" onclick="changeMain(this.id)">Partidas</div>
-    <div class="lcOption title" id="Personajes" onclick="changeMain(this.id)">Personajes</div>
-    <div class="lcOption title" id="Habilidades" onclick="changeMain(this.id)">Habilidades</div>
-    <div class="lcOption title" id="Apuntes" onclick="changeMain(this.id)">Apuntes</div>
+    <img class="lbButton" onclick="changeMain('Partidas')" width="20px" height="20px"  src="../../resources/imgs/sections/dado.png" alt=""><div class="lcOption title" id="Partidas" onclick="changeMain(this.id)">Partidas</div>
+    <img class="lbButton" onclick="changeMain('Personajes')" width="20px" height="20px"  src="../../resources/imgs/sections/char.png" alt=""><div class="lcOption title" id="Personajes" onclick="changeMain(this.id)">Personajes</div>
+    <img class="lbButton" onclick="changeMain('Habilidades')" width="20px" height="20px"  src="../../resources/imgs/sections/generico.png" alt=""><div class="lcOption title" id="Habilidades" onclick="changeMain(this.id)">Habilidades</div>
+    <img class="lbButton" onclick="changeMain('Apuntes')" width="20px" height="20px"  src="../../resources/imgs/sections/notes.png" alt=""><div class="lcOption title" id="Apuntes" onclick="changeMain(this.id)">Apuntes</div>
 </div>
