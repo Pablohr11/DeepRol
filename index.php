e51720f807a070cdd6a9540cfd17a5c7e7ae5c52
Pablo <pablo993968@gmail.com>
2025-07-14 17:57:48 +0200

    uy

--- index.php
+++ index.php
@@ -84,6 +84,56 @@
         // document.getElementById(targetContainerId).style.display = "block";
     }
 </script>
+<div style="position: fixed; inset: 0; z-index: -1; overflow: hidden;">
+  <svg width="100%" height="100%" viewBox="0 0 1440 900" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <!-- Gradiente ámbar -->
+    <linearGradient id="ondaAmber" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="0%" style="stop-color:#e2a349;stop-opacity:0.06" />
+      <stop offset="100%" style="stop-color:#ffcc70;stop-opacity:0.04" />
+    </linearGradient>
 
+    <!-- Niebla: patrón de círculos difuminados -->
+    <radialGradient id="fogCircle" cx="50%" cy="50%" r="50%">
+      <stop offset="0%" style="stop-color:#e2a349;stop-opacity:0.03" />
+      <stop offset="100%" style="stop-color:#000000;stop-opacity:0" />
+    </radialGradient>
+  </defs>
+
+  <!-- Ondas -->
+  <path fill="url(#ondaAmber)">
+    <animate attributeName="d" dur="12s" repeatCount="indefinite"
+      values="
+        M0,320 Q360,420 720,320 T1440,330 V900 H0 Z;
+        M0,340 Q360,300 720,340 T1440,350 V900 H0 Z;
+        M0,320 Q360,420 720,320 T1440,330 V900 H0 Z" />
+  </path>
+
+  <path fill="url(#ondaAmber)">
+    <animate attributeName="d" dur="18s" repeatCount="indefinite"
+      values="
+        M0,480 Q360,580 720,460 T1440,480 V900 H0 Z;
+        M0,500 Q360,450 720,500 T1440,490 V900 H0 Z;
+        M0,480 Q360,580 720,460 T1440,480 V900 H0 Z" />
+  </path>
+
+  <!-- Niebla animada -->
+  <g>
+    <circle cx="200" cy="300" r="250" fill="url(#fogCircle)">
+      <animateTransform attributeName="transform" type="translate"
+        values="0,0; 20,-10; 0,0" dur="15s" repeatCount="indefinite"/>
+    </circle>
+    <circle cx="800" cy="500" r="300" fill="url(#fogCircle)">
+      <animateTransform attributeName="transform" type="translate"
+        values="0,0; -30,20; 0,0" dur="20s" repeatCount="indefinite"/>
+    </circle>
+    <circle cx="1200" cy="250" r="200" fill="url(#fogCircle)">
+      <animateTransform attributeName="transform" type="translate"
+        values="0,0; 15,-25; 0,0" dur="22s" repeatCount="indefinite"/>
+    </circle>
+  </g>
+</svg>
+
+</div>
 </body>
 </html>
--- sections/_partials/leftColumn.php
+++ sections/_partials/leftColumn.php
@@ -1,7 +1,12 @@
 <div id="leftColumnContent">
-    <img id="toogleSideBarButton" width="20px" height="20px"  src="../../resources/imgs/collapse.png" alt="">
-    <img class="lbButton" onclick="changeMain('Partidas')" width="20px" height="20px"  src="../../resources/imgs/sections/dado.png" alt=""><div class="lcOption title" id="Partidas" onclick="changeMain(this.id)">Partidas</div>
-    <img class="lbButton" onclick="changeMain('Personajes')" width="20px" height="20px"  src="../../resources/imgs/sections/char.png" alt=""><div class="lcOption title" id="Personajes" onclick="changeMain(this.id)">Personajes</div>
-    <img class="lbButton" onclick="changeMain('Habilidades')" width="20px" height="20px"  src="../../resources/imgs/sections/generico.png" alt=""><div class="lcOption title" id="Habilidades" onclick="changeMain(this.id)">Habilidades</div>
-    <img class="lbButton" onclick="changeMain('Apuntes')" width="20px" height="20px"  src="../../resources/imgs/sections/notes.png" alt=""><div class="lcOption title" id="Apuntes" onclick="changeMain(this.id)">Apuntes</div>
+    <div id="leftColumnContentUpper">    
+        <img id="toogleSideBarButton" width="20px" height="20px"  src="../../resources/imgs/collapse.png" alt="">
+        <img class="lbButton" onclick="changeMain('Partidas')" width="20px" height="20px"  src="../../resources/imgs/sections/dado.png" alt=""><div class="lcOption title" id="Partidas" onclick="changeMain(this.id)">Partidas</div>
+        <img class="lbButton" onclick="changeMain('Personajes')" width="20px" height="20px"  src="../../resources/imgs/sections/char.png" alt=""><div class="lcOption title" id="Personajes" onclick="changeMain(this.id)">Personajes</div>
+        <img class="lbButton" onclick="changeMain('Habilidades')" width="20px" height="20px"  src="../../resources/imgs/sections/generico.png" alt=""><div class="lcOption title" id="Habilidades" onclick="changeMain(this.id)">Habilidades</div>
+        <img class="lbButton" onclick="changeMain('Apuntes')" width="20px" height="20px"  src="../../resources/imgs/sections/notes.png" alt=""><div class="lcOption title" id="Apuntes" onclick="changeMain(this.id)">Apuntes</div>
+    </div>
+    <div id="leftColumnContentUpper">
+        <img class="lbButton" onclick="changeMain('Apuntes')" width="20px" height="20px"  src="../../resources/imgs/sections/Ajustes.png" alt=""><div class="lcOption title" id="Ajustes" onclick="changeMain(this.id)">Ajustes</div>
+    </div>
 </div>
